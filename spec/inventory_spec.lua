0404fedc4f4b97dbbd0f467b4d0399f362ab477e
Matteo-Penaud <72443905+Matteo-Penaud@users.noreply.github.com>
2026-03-09 20:28:09 +0100

    Added items in inventory

--- spec/inventory_spec.lua
+++ spec/inventory_spec.lua
@@ -1,11 +1,13 @@
 local Inventory = require("src.inventory")
+local Item = require("src.item")
 
 describe("Inventory utility :", function()
     describe("Create an inventory representation", function()
         it("Simple creation", function()
-            local inventory = Inventory.new("inventory_1")
+            local inventory = Inventory.new("inventory_1", 1)
             
             assert.are.same("inventory_1", inventory.name)
+            assert.are.same(1, inventory.size)
         end)
 
         -- Test name parameter
@@ -18,9 +20,52 @@ describe("Inventory utility :", function()
         for _, case in pairs(invalidNames) do
             it(("Name is %s"):format(case[1]), function()
                 assert.has_error(function()
-                    Inventory.new(case[2])
+                    Inventory.new(case[2], 1)
                 end, "Inventory name must be a non-empty string")
             end)
         end
+
+        -- Test invalid size parameter
+        local invalidSize = {
+            {"nil", nil},
+            {"not a number", "test"},
+            {"0", 0},
+            {"negative", -1}
+        }
+
+        for _, case in pairs(invalidSize) do
+            it(("Size is %s"):format(case[1]), function()
+                assert.has_error(function()
+                    Inventory.new("toto", case[2])
+                end, "Size must be a positive not 0 integer")
+            end)
+        end
+    end)
+
+    describe("Add items to the inventory", function()
+        it("Add a simple item", function()
+            local inventory = Inventory.new("inventory_1", 1)
+            local item = Item.new("toto", 10, 64)
+
+            inventory:addItem(item, 1)
+            assert.are.same(item, inventory.items[1])
+        end)
+
+        it("Multiple items", function()
+            local inventory = Inventory.new("inventory_1", 1)
+            local items = {
+                Item.new("toto", 10, 64),
+                Item.new("tata", 1, 2),
+                Item.new("titi", 1, 2)
+            }
+
+            for slot, item in pairs(items) do
+                inventory:addItem(item, slot)
+            end
+
+            for slot, item in pairs(items) do
+                assert.are.same(item, inventory.items[slot])
+            end
+        end)
     end)
 end)
--- src/inventory.lua
+++ src/inventory.lua
@@ -1,13 +1,14 @@
+local Item = require("src.item")
+
 local Inventory = {}
 
 Inventory.__index = Inventory
 
 --- Create a new inventory.
 --- @param name string
+--- @param size number
 --- @return table
-function Inventory.new(name)
-    local self = setmetatable({}, Inventory)
-
+function Inventory.new(name, size)
     if not name
         or type(name) ~= "string"
         or name == ""
@@ -15,9 +16,29 @@ function Inventory.new(name)
         error("Inventory name must be a non-empty string")
     end
 
+    if not size
+        or type(size) ~= "number"
+        or size <= 0
+    then
+        error("Size must be a positive not 0 integer")
+    end
+
+    local self = setmetatable({}, Inventory)
+
     self.name = name
+    self.size = size
+
+    self.items = {}
     
     return self
+end
+
+--- Create a new inventory.
+--- @param item table
+--- @param slot number
+--- @return nil
+function Inventory:addItem(item, slot)
+    table.insert(self.items, slot, item)
 end
 
 return Inventory
